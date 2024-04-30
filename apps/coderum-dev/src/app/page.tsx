f8651c209e858b5107334dc3e2405af9a6bebb4d
Minh <cungminh27@gmail.com>
2024-05-01 00:03:50 +1000

    edge runtime

--- apps/coderum-dev/src/app/page.tsx
+++ apps/coderum-dev/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 import { api } from '@/trpc/server';
+export const runtime = 'edge';
 
 export default async function Home() {
 	const hello = await api.post.hello({ text: 'from tRPC' });
