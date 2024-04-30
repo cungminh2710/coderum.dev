f341c11df610a6a88c3c43a59921a582a1ab2c2d
Minh <cungminh27@gmail.com>
2024-05-01 00:20:15 +1000

    test build

--- apps/coderum-dev/src/app/api/trpc/[trpc]/route.ts
+++ apps/coderum-dev/src/app/api/trpc/[trpc]/route.ts
@@ -5,7 +5,7 @@ import { env } from '@/env';
 import { appRouter } from '@/server/api/root';
 import { createTRPCContext } from '@/server/api/trpc';
 
-export const config = { runtime: 'edge' };
+export const runtime = 'edge';
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
